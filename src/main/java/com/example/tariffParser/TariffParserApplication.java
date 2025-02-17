1b4cc372ae878dadb2fa331aeab7c18603fa9aa4
Ditamin <tagirovdinar1@gmail.com>
2025-02-17 11:20:54 +0500

    init

---
+++ src/main/java/com/example/tariffParser/TariffParserApplication.java
@@ -0,0 +1,13 @@
+package com.example.tariffParser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TariffParserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TariffParserApplication.class, args);
+	}
+
+}
